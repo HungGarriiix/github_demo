c6c6689b645b24f2728b409286354753cd00c7d6
HungGarriiix <crazyhung060@gmail.com>
2025-01-14 11:07:29 +0700

    adds new file

---
+++ Main.lua
@@ -0,0 +1,20 @@
+require("Counter")
+require("Clock")
+
+local clock = Clock:new(nil)
+
+local function PrintClock(clock)
+    os.execute("cls")
+    print("Clock: ")
+    print(clock:GetCurrentTime())
+end
+
+local function Main()
+    for i = 0, 36000, 1
+    do
+        clock:Tick()
+        PrintClock(clock)
+    end
+end
+
+Main()
